a50b568540b4a511a4c8c80bfce6dc7e24f351d2
Mohamed Salah MEDDEB <medsalah.meddeb@gmail.com>
2021-01-20 22:28:08 +0100

    Ajout du support pour la formation Devops

---
+++ App_Test_Maven_Sonarqube/src/main/java/org/gk/cursus-devops/App.java
@@ -0,0 +1,13 @@
+package org.gk.cursus-devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
